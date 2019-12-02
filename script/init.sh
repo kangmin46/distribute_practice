de24186967d14876fd2e68ee1ec76f388dc62be0
kangmin46 <kangmin789@naver.com>
2019-12-02 14:46:09 +0900

    refactor : modify init.sh

--- script/init.sh
+++ script/init.sh
@@ -1,5 +1,5 @@
-sudo su
-snap install docker --devmode
-git clone -b build https://github.com/kangmin46/dp.git
+
+sudo snap install docker --devmode
+sudo git clone -b build https://github.com/kangmin46/dp.git
 cd dp
-docker build -t Hello .
+sudo docker build -t Hello .
