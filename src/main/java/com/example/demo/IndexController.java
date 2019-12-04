eb1c25a4c58c291d2d741421348b913aad628907
kangmin46 <kangmin789@naver.com>
2019-12-04 15:12:22 +0900

    test

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -5,12 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
-    @GetMapping("/abc")
-    public String abc() {
-        return "abc.html";
-    }
-
     @GetMapping("/")
     public String index() {
         return "index.html";
