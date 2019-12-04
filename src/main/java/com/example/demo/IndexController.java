e185735ed6b59a7cae1030f58ca2305c2f2ebe72
kangmin46 <kangmin789@naver.com>
2019-12-04 17:52:50 +0900

    test

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -9,4 +9,9 @@ public class IndexController {
     public String index() {
         return "index.html";
     }
+
+    @GetMapping("/abc")
+    public String abc() {
+        return "abc.html";
+    }
 }
